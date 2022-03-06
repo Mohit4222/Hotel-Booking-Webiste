73e171639e59d424e1ec086365cdd6513aed853a
Mohit <mohitsharma4222@gmail.com>
2022-03-06 19:51:16 +0530

    Checkpoint 11

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+
+
+let ViewMoreCity = () => {
+    let viewMoreButton = document.getElementsByClassName('btn btn-secondary')[0];
+    if (viewMoreButton.innerText === "View More") {
+        document.getElementById("addedCityCards").style.display="block";
+        viewMoreButton.innerText ='View Less';
+    }
+    else {
+        document.getElementById("addedCityCards").style.display="none";
+        viewMoreButton.innerText ='View More';
+    }
+}
